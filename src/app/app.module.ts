a900754d118b40e9b57c74a5a0cb60fceb1c71c4
Ashesh <83585384+ashesh790@users.noreply.github.com>
2023-07-15 02:08:18 +0530

    Publish parth plastomech at initial.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
+import { ImgSliderComponent } from './img-slider/img-slider.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ChooseComponent } from './choose/choose.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent,
+    ImgSliderComponent,
+    AboutComponent,
+    ProductComponent,
+    ChooseComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
