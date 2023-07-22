a0fdd926bd35024f69945a9454a82929894bce70
Ashesh790 <asheshtparmar.com>
2023-07-23 00:14:41 +0530

    Routing added for add products.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ProductComponent } from './product/product.component';
 import { MasterComponent } from './master/master.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProductCrudComponent } from './product-crud/product-crud.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent }, 
+  { path: 'add_products', component: ProductCrudComponent }
 ];
 
 @NgModule({
