7c5290b344b9f2d400b084a020c509fb5cdeab1d
Ashesh790 <asheshtparmar.com>
2023-07-26 00:23:47 +0530

    Key value added as a service for product name.

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,5 +9,29 @@ export class AppService {
   getProductData(){
     let productUrl = "http://localhost:8085/api/products"; 
     return this.http.get(productUrl);
-  }
+  } 
+
+  navData = {
+    "Plastic Extrusion Plants":{
+      "PP Tape Fibrillated Extrusion Plant":"PP Tape Fibrillated Extrusion Plant", 
+      "PP / HDPE Monofilament Extrusion Yarn Plant":"PP / HDPE Monofilament Extrusion Yarn Plant",       
+      "Mono Danline Extrusion Plant":"Mono Danline Extrusion Plant", 
+      "Danline Extrusion Plant":"Danline Extrusion Plant",
+      "Baler Twine Extrusion Plant":"Baler Twine Extrusion Plant", 
+      "Sutil Extrusion Plant":"Sutil Extrusion Plant"
+    }, 
+    "Rope Making Machine":{
+      "Rope Making Machine 2-6mm":"Rope Making Machine 2-6mm",
+      "Rope Making Machine 4-8mm":"Rope Making Machine 4-8mm",  
+      "Rope Making Machine 6-12mm":"Rope Making Machine 6-12mm",  
+      "Rope Making Machine 8-16mm":"Rope Making Machine 8-16mm",  
+      "Rope Making Machine 12-24mm":"Rope Making Machine 12-24mm", 
+      "Rope Making Machine 18-40mm":"Rope Making Machine 18-40mm"
+    }, 
+    "Winder":{
+      "Bobbin Winder":"Bobbin Winder", 
+      "Cheeze Winde":"Cheeze Winde", 
+      "Roll Winder":"Roll Winder"
+    }
+  } 
 }
--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
   styleUrls: ['./master.component.css']
 })
-export class MasterComponent {
+export class MasterComponent implements OnInit{
+  constructor(public navData:AppService){}
 
+  ngOnInit(): void {
+    
+  }
 }
